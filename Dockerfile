9be4a9da503067d3a0f06a8ac08dbbf1be31d9fb
javidy <javid.yusifov@yahoo.com>
2020-06-20 23:42:54 +0200

    enabled remote autoreload functionality using springbootdevtools

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ RUN mkdir /home/app
 WORKDIR /home/app
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
-COPY build/libs/*.jar app.jar
 ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/tasker/server/rest/TestRestController.java
@@ -0,0 +1,13 @@
+package com.tasker.server.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "hello wodsfdsrld";
+    }
+}
