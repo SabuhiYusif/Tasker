43953263ddd9e6d4c840a03ec9bcf2a3be45067f
javidy <javid.yusifov@yahoo.com>
2020-07-14 20:39:58 +0200

    delete /boards endpoint

---
+++ src/main/java/com/tasker/server/boards/rest/BoardDeletionController.java
@@ -0,0 +1,34 @@
+package com.tasker.server.boards.rest;
+
+import com.tasker.server.boards.Board;
+import com.tasker.server.boards.rest.services.BoardService;
+import com.tasker.server.rest.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class BoardDeletingController {
+
+    @Resource
+    BoardService boardService;
+
+    @DeleteMapping("/boards/{id}")
+    ResponseEntity<?> delete(@PathVariable String id) {
+
+        UUID boardId = UUID.fromString(id);
+        Board board = boardService.fetch(boardId);
+        if (board == null) {
+            throw new RecordNotFoundException("Board not found for id - " + id);
+        }
+        boardService.delete(boardId);
+        return ResponseEntity.ok("Board Deleted");
+    }
+}
